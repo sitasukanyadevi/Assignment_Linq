f0851bc1b040a5377f8c806d1d8a72ff634b31ba
Bande <s.bande@dxc.com>
2023-05-03 21:56:13 +0530

    updated

--- Linq_Exercise/Linq_Exercise/Program.cs
+++ Linq_Exercise/Linq_Exercise/Program.cs
@@ -1,39 +1,41 @@
-﻿namespace linq_excercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace linq_excercise
 {
-
-    public class Product
+    internal class Program
     {
-        public string ProductId { get; set; }
-        public string ProductName { get; set; }
-        public string Brand { get; set; }
-        public int Quantity { get; set; }
-        public int Price { get; set; }
-    }
-
-    public class Program
-    {
-        static List<Product> productList = new List<Product>();
-
-        public void SeedData()
+        class Product
         {
-            productList.Add(new Product { ProductId = "P001", ProductName = "Laptop", Brand = "Dell", Quantity = 5, Price = 35000 });
-            productList.Add(new Product { ProductId = "P002", ProductName = "Camera", Brand = "Canon", Quantity = 8, Price = 28500 });
-            productList.Add(new Product { ProductId = "P003", ProductName = "Tablet", Brand = "Lenovo", Quantity = 4, Price = 15000 });
-            productList.Add(new Product { ProductId = "P004", ProductName = "Mobile", Brand = "Apple", Quantity = 9, Price = 65000 });
-            productList.Add(new Product { ProductId = "P005", ProductName = "Earphones", Brand = "Boat", Quantity = 2, Price = 1500 });
-        }
+            public string ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string Brand { get; set; }
+            public int Quantity { get; set; }
+            public int Price { get; set; }
+            public void SeedData(List<Product> productList)
 
+            {
+               productList.Add(new Product { ProductId = "P001", ProductName = "Laptop", Brand = "Dell", Quantity = 5, Price = 35000 });
+               productList.Add(new Product { ProductId = "P002", ProductName = "Camera", Brand = "Canon", Quantity = 8, Price = 28500 });
+               productList.Add(new Product { ProductId = "P003", ProductName = "Tablet", Brand = "Lenovo", Quantity = 4, Price = 15000 });
+               productList.Add(new Product { ProductId = "P004", ProductName = "Mobile", Brand = "Apple", Quantity = 9, Price = 65000 });
+               productList.Add(new Product { ProductId = "P005", ProductName = "Earphones", Brand = "Boat", Quantity = 2, Price = 1500 });
+               
+            }
+            
+
+        }
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.SeedData();
+            List<Product> productList = new List<Product>();
+            Product ob = new Product();
+            ob.SeedData(productList);
 
             //1
-            var query = from p in productList where p.Price >= 20000 && p.Price <= 40000 select p.ProductName;
+            var query = from p in productList where p.Price >= 20000 && p.Price <= 40000 select p;
             Console.WriteLine("Product names where price is between 20000 and 40000:");
             foreach (var x in query)
             {
-                Console.WriteLine(x);
+                Console.WriteLine(x.ProductName);
             }
             Console.WriteLine();
 
